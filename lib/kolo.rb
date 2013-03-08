20eba878b5f060de335d1157c726b191f32ca8a9
Rahoul Baruah <rahoul@3hv.co.uk>
2013-03-08 18:05:44 +0000

    Shoot me

--- lib/kolo.rb
+++ lib/kolo.rb
@@ -1,7 +1,7 @@
 require "kolo/version"
 
 module Kolo
-  class Engine < ::Rails::Enging
+  class Engine < ::Rails::Engine
 
   end
 end
--- lib/kolo/version.rb
+++ lib/kolo/version.rb
@@ -1,3 +1,3 @@
 module Kolo
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
